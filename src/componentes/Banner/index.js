4cc17c13d0ce6cd5f6a080a76b210c410c1fcd3b
Caio Cabral <ocaiocabral@gmail.com>
2022-12-10 14:40:57 -0400

    credit line

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -4,16 +4,20 @@ function Banner(){
 
    return(
       <header className={'header'}>      
-         <div className='credit-container'>
-            <span className={'credit-vertical'}>Photo by</span>         
-            <div className='credit-line'>
-               <a href={"https://unsplash.com/@joewthompson?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Joey Thompson</a>
-               <span className={'header__credit'}>on</span> 
-               <a href={"https://unsplash.com/s/photos/Festival?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Unsplash</a>
-               </div>         
-            </div>
-         
-        
+         <p className='credit'>
+            <span>Photo by</span>
+            <a href={"https://unsplash.com/@joewthompson?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Joey Thompson</a>
+            <span>on</span> 
+            <a href={"https://unsplash.com/s/photos/Festival?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Unsplash</a>
+            <span>Photo by</span>
+            <a href={"https://unsplash.com/@joewthompson?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Joey Thompson</a>
+            <span>on</span> 
+            <a href={"https://unsplash.com/s/photos/Festival?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Unsplash</a>
+            <span>Photo by</span>
+            <a href={"https://unsplash.com/@joewthompson?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Joey Thompson</a>
+            <span>on</span> 
+            <a href={"https://unsplash.com/s/photos/Festival?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Unsplash</a>
+         </p>
       </header>
 
    )
