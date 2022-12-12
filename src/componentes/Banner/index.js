5a9e3e9c5ac55bb1b1d9fd580297d6502498cb8b
Caio Cabral <ocaiocabral@gmail.com>
2022-12-11 20:58:20 -0400

    Slider animation

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -4,20 +4,14 @@ function Banner(){
 
    return(
       <header className={'header'}>      
-         <p className='credit'>
-            <span>Photo by</span>
-            <a href={"https://unsplash.com/@joewthompson?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Joey Thompson</a>
-            <span>on</span> 
-            <a href={"https://unsplash.com/s/photos/Festival?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Unsplash</a>
-            <span>Photo by</span>
-            <a href={"https://unsplash.com/@joewthompson?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Joey Thompson</a>
-            <span>on</span> 
-            <a href={"https://unsplash.com/s/photos/Festival?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Unsplash</a>
-            <span>Photo by</span>
-            <a href={"https://unsplash.com/@joewthompson?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Joey Thompson</a>
-            <span>on</span> 
-            <a href={"https://unsplash.com/s/photos/Festival?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Unsplash</a>
-         </p>
+         <div className={'credit'}>
+            <p className={'credit__line'}>
+               Photo by <a href={"https://unsplash.com/@joewthompson?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Joey Thompson</a> on <a href={"https://unsplash.com/s/photos/Festival?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Unsplash</a>
+            </p>
+            <p className={'credit__line second'}>
+               Photo by <a href={"https://unsplash.com/@joewthompson?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Joey Thompson</a> on <a href={"https://unsplash.com/s/photos/Festival?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"}>Unsplash</a>
+            </p>
+         </div>
       </header>
 
    )
