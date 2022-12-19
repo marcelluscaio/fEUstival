553cfe563205cab6a53d4121db59395a3ff32c16
Caio Cabral <ocaiocabral@gmail.com>
2022-12-18 20:09:46 -0400

    Change in background color

--- src/componentes/Palcos/index.js
+++ src/componentes/Palcos/index.js
@@ -4,7 +4,7 @@ import Banda from '../Banda';
 
 const Palcos = (props) => {
    return(
-      props.bandas.length > 0 && <section className='palco' style={{backgroundColor: props.cor2}}>
+      props.bandas.length > 0 && <section className='palco' style={{backgroundColor: props.cor1}}>
          <h3 className='palco__titulo' style={{borderColor: props.cor1, color: props.cor1}}>{props.nome}</h3>
          {/* <Colaborador /> */}
          <div className='palco__bandas'>
