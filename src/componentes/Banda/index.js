1f324ce7ca8c2e11e899cf829a2cfc2dfb46e5ee
Caio Cabral <ocaiocabral@gmail.com>
2022-12-19 17:19:12 -0400

    ADding trash can using react-icons

--- src/componentes/Banda/index.js
+++ src/componentes/Banda/index.js
@@ -1,8 +1,9 @@
 import './banda.css';
+import { BiTrash } from "react-icons/bi";
 
 const Banda = ({banda, imagem, cor, genero}) => {
    return(
-      <div className='banda'>
+      <div className='banda'>         
          <div className='banda__cabecalho' style={{backgroundColor: cor}}>
             <img className={'cabecalho__imagem'} src={imagem} alt={banda}/>
          </div>
@@ -10,6 +11,7 @@ const Banda = ({banda, imagem, cor, genero}) => {
             <h4 className={'rodape__nome'} style={{color: cor}}>{banda}</h4>
             <h5 className={'rodape__genero'} style={{color: cor}}>{genero}</h5>
          </div>
+         <BiTrash className={'banda__excluir'} fill={cor} />
       </div>
    
    );
